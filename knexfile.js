34b4e88608e0cfe11cf4bc5709b0cefb3259a87d
Marina Monsivais <minamonsi@me.com>
2021-11-14 22:51:46 -0600

    Added knexfile.js  & db-config file

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const sharedConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations' },
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  }
+  
+  module.exports = {
+    development: {
+      ...sharedConfig,//using sharedConfig info
+      connection: { filename: './data/fitness.db3' },
+      seeds: { directory: './data/seeds' },
+      },
+      testing: {
+        ...sharedConfig,//copying & using sharedConfig info
+        connection: { filename: './data/fitness_test.db3' },
+      },
+  
+  };
+  
