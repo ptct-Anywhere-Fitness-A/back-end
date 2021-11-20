6253144d78c0e69d3631e2b21c79d55352843c65
Marina Monsivais <minamonsi@me.com>
2021-11-19 21:37:22 -0600

    register endpoint completed

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -9,6 +9,30 @@ const checkLogin = require('./middleware/checkLogin');
 
 const secret = process.env.SECRET || "aklsjfsolidflkajddeoalkdjfdgratciaskfdj"
 
+function makeToken(user){
+    const payload = {
+      subject:user.id,
+      username:user.username
+    }
+    const options = {
+      expiresIn: "1d"
+    }
+    return jwt.sign(payload, secret, options)
+}
+
+router.post('/register', uniqueUsername, checkLogin, async (req,res) => {
+    try {
+        const {username, password, role} = req.body
+        const newUser = await Users.addUser({
+            username,
+            password: bcrypt.hashSync(password, 8),
+            role,
+        })
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 
 module.exports = router;
